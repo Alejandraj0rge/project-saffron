b06cf86aba89ef576817cc7bb1ba9f6f40578955
Alejandraj0rge <alejandra.jorge@hytechgizmo.com>
2022-08-16 08:45:01 -0400

    side-menu component

--- resources/views/tasks/dashboard.blade.php
+++ resources/views/tasks/dashboard.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-    
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
     <h1>Hello, world!</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <div id="app">
-        <div id="side-menu">
-        </div>
-        <div id="main">
-        </div>
-    </div>
+        {{-- <example-component></example-component> --}}
+        <side-menu></side-menu>
+    </div> 
 </body>
 
 </html>
