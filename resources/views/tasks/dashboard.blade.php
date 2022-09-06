d3e1e42a1137c8b76da46e7ecdcefa6bff7f71fe
Alejandraj0rge <alejandra.jorge@hytechgizmo.com>
2022-09-06 16:55:28 -0400

    modaling

--- resources/views/tasks/dashboard.blade.php
+++ resources/views/tasks/dashboard.blade.php
@@ -59,10 +59,15 @@
 			
 			<div style="display: flex; justify-content:space-between;">
 				<h4>Home</h4>
-				<div>
-					<input type="text" placeholder="Search">
-					<button id="button">Upgrade</button>
-					<button id="plus-button">+</button>
+				<div style="display: flex;justify-content:space-between;align-items:center">
+					<div style="display: flex;">
+						<div id="plus-button" style="margin-right: 10px"><span class="material-icons">add</span></div>
+						<input type="text" id="search-task" placeholder="Search a task!">
+					</div>
+					<div style="margin-left: 20px;display: flex;">
+						<div id="plus-button" style="margin-right: 10px;background-color:lightgreen"></div>
+						<button class="btn btn-outline-light" style="font-size: small;" data-bs-toggle="modal" data-bs-target="#signInModal">Sign out</button>
+					</div>
 				</div>
 			</div>
 
@@ -71,10 +76,10 @@
 				<h2>Good afternoon, Alejandra</h2>
 				<div style="display:flex;justify-content:center;flex-direction: column;">
 					<div>
-						<div id="recap-bar">
+						<div id="recap-bar" style="display:flex;align-items:center;">
 							<div style="margin-right:20px;">My week</div>
-							<div style="margin-right:20px;">0 tasks completed</div>
-							<div>0 collaborators</div>
+							<div style="margin-right:20px;display:flex;align-items:center"><h3 style="margin-right:10px;">0</h3> tasks completed</div>
+							<div style="display:flex;align-items:center;"><h3 style="margin-right:10px;">0</h3> collaborators</div>
 						</div>
 					</div>
 					<div>Customize</div>
@@ -95,13 +100,13 @@
 						<hr>
 						<div>
 							<ul style="list-style-type:none;padding:20px">
-								<li class="muted"><span class="material-icons">check_circle</span>Click here to add a task</li>
+								<li class="muted" style="align-items: center;display:flex;"><span class="material-icons" style="margin-right:5px;">check_circle</span><p contenteditable="true" style="margin:0;outline: 0px solid transparent;">Click here to add a task</p></li>
 								<hr style="margin: 10px 0">
-								<li><span class="material-icons">check_circle</span>Click here to add a task</li>
+								<li><span style="margin-right:5px;" class="material-icons">check_circle</span>Click here to add a task</li>
 								<hr style="margin: 10px 0">
-								<li><span class="material-icons">check_circle</span>Click here to add a task</li>
+								<li><span style="margin-right:5px;" class="material-icons">check_circle</span>Click here to add a task</li>
 								<hr style="margin: 10px 0">
-								<li><span class="material-icons">check_circle</span>Click here to add a task</li>
+								<li><span style="margin-right:5px;" class="material-icons">check_circle</span>Click here to add a task</li>
 								<hr style="margin: 10px 0">
 							</ul>
 						</div>
@@ -180,122 +185,6 @@
 				</div>
 			</div>
 		</div>
-		{{-- <top-header></top-header> --}}
-		
-{{-- 		<div class="row row-cols-3" style="height:100vh;">
-			
-			<div class="col" style="padding:20px;width:max-content;padding-left:30px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);border-radius:10px;">
-				<side-menu></side-menu>
-				<ul style="list-style-type: none;margin: 0;padding: 0;">
-					<li><span class="material-icons">menu</span></li>
-					<li><span class="material-icons">check_box_outline_blank</span></li>
-					<li><span class="material-icons">radio_button_unchecked</span></li>
-						</span>
-				</ul>
-				 --}}
-				
-					{{-- <div>
-						<span class="material-symbols-outlined" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">menu</span>
-				
-						<div style="min-height: 120px;">
-							<div class="collapse collapse-horizontal" id="collapseWidthExample">
-								<div class="card" style="width:max-content;">
-									<ul class="list-group">
-										<span v-html="options"></span>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div> --}}
-{{-- 			</div>
-
-        	<div class="col">
-				<main-card></main-card>
-          	</div>
-
-			<div class="col-sm-6">
-                <div class="container"  style="padding:20px;" data-masonry='{"percentPosition": true }'>
-					<div class="row row-cols-2">
-
-						<div class="col-sm-auto" style="margin:10px;">
-							<div class="card" style="width: 35rem;height:30rem">
-								<div class="row justify-content-center" style="padding: 40px;">
-									<div class="card" style="width: max-content;height:max-content;">
-										<div class="hstack gap-3">
-											First item
-											<div class="vr"></div>
-											0 tasks completed
-											<div class="vr"></div>
-											0 collaborators
-										</div>
-									</div>
-									<div>
-										<h3 class="text-center" style="padding:10px">My Tasks</h3>
-										<ul class="nav">
-											<li class="nav-item">
-											  <a class="nav-link active" aria-current="page" href="#">Upcoming</a>
-											</li>
-											<li class="nav-item">
-											  <a class="nav-link" href="#">whatt</a>
-											</li>
-											<li class="nav-item">
-											  <a class="nav-link" href="#">Link</a>
-											</li>
-											<li class="nav-item">
-											  <a class="nav-link disabled">Disabled</a>
-											</li>
-										  </ul>
-									</div>
-									<div>
-										<ul class="list-group">
-											<li class="list-group-item text-muted" contenteditable="true">Write something funny!</li>
-											<li class="list-group-item">
-											  <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox">
-											  <label class="form-check-label" for="firstCheckbox">First checkbox</label>
-											</li>
-											<li class="list-group-item">
-											  <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox">
-											  <label class="form-check-label" for="secondCheckbox">Second checkbox</label>
-											</li>
-											<li class="list-group-item">
-											  <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox">
-											  <label class="form-check-label" for="thirdCheckbox">Third checkbox</label>
-											</li>
-										  </ul>
-									</div>
-								</div>
-							</div>
-						</div>
-						
-						<div class="col-sm-auto" style="margin:10px;">
-							<div class="card" style="width:18rem;height:30rem">
-								<div class="card-body">
-								  <h5 class="card-title" contenteditable="true">New Note</h5>
-								  <h6 class="card-subtitle mb-2 text-muted" contenteditable="true">Note subtitle</h6>
-								  <p class="card-text" contenteditable="true">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								</div>
-								<div class="card-footer d-flex justify-content-between">
-										<div class="col-4">
-											<span class="material-icons">skateboarding</span>
-											<span class="material-icons">color_lens</span>
-											<span class="material-icons">photo_camera_back</span>
-										</div>
-										
-										<div class="col-1">
-											<span class="material-icons">done_outline</span>
-										</div>
-										
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-            </div>
-
-        </div> --}}
-
 	</div>
 </body>
 
